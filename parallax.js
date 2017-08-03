a1712749b8a19a69cf825515fb80203d566389a9
Misa Jokisalo <misa.jokisalo@gmail.com>
2017-08-03 20:06:59 +0300

    Why are these files here?

--- parallax.js
+++ parallax.js
@@ -1,4 +1,4 @@
-let elements = $('body').find('.parallax-component');
+let elements = [];
 
 // window.addEventListener(
 //   'scroll',
@@ -10,14 +10,24 @@ let elements = $('body').find('.parallax-component');
 //   (event) => requestAnimationFrame(() => updateHeaderHeight())
 // );
 
+window.onload = function() {
+	svgObject = document.getElementById('mountainscape').contentDocument;
+	svg_objects = $(svgObject).find('.parallax-component');
+	header_objects = $('header').find('.parallax-component');
+	elements = concat_collection(svg_objects, header_objects);
+	updateHeaderHeight();
+	sortZIndexByDepth(elements);
+};
+
+
 $(window).on('scroll', function() {
-   window.requestAnimationFrame(updateParallaxOffset);
+   	window.requestAnimationFrame(updateParallaxOffset);
 });
+
 
 $(window).on('resize', function() {
    window.requestAnimationFrame(updateHeaderHeight);
 });
-
 
 
 function updateParallaxOffset() {
@@ -30,13 +40,9 @@ function updateParallaxOffset() {
 		if(elements[i].getAttribute('id') == 'trees-front') {
 			y_offset = y_offset + 5;
 		}
-		$(elements[i]).css('transform', `translate(-50%, ${y_offset}px)`);
+		$(elements[i]).css('transform', `translateY(${y_offset}px)`);
 	}
 }
-
-$('#background').ready(function() {
-	updateHeaderHeight();
-});
 
 //	Update header height to the background parallax layer's height
 function updateHeaderHeight() {
@@ -49,3 +55,23 @@ function updateHeaderHeight() {
 	let height = Math.ceil($(window).width() * ratio);
 	$('header').height(height + "px");
 };
+
+function concat_collection(obj1, obj2) {
+    var i;
+    var arr  = new Array();
+    var len1 = obj1.length;
+    var len2 = obj2.length;
+    for (i=0; i<len1; i++) {
+        arr.push(obj1[i]);
+    }
+    for (i=0; i<len2; i++) {
+        arr.push(obj2[i]);
+    }
+    return arr;
+}
+
+function sortZIndexByDepth(elements) {
+	for(let i = 0; i < elements.length; i++) {
+		console.log($(elements[i]).attr('data-depth'));
+	}
+}
