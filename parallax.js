1b410ef81dead0acfbf49b2c572889039f71f40f
Misa Jokisalo <misa.jokisalo@gmail.com>
2017-08-05 18:57:57 +0300

    WIP: Parallax only in viewport works

--- parallax.js
+++ parallax.js
@@ -41,14 +41,19 @@ $(window).on('resize', function() {
 
 
 function updateParallaxOffset() {
-	let offset = window.pageYOffset
+	let offset = window.pageYOffset //	Get offset from top of parallax container, not entire page.
 
 	//	Degub array for in / out of viewport
 	var in_or_out = [];
 	var debug_string = "";
 
+
+
 	for(let i = 0; i < elements.length; i++) {
 		let container = $(elements[i]).parents('.parallax-container')[0];
+		let offset = container.getBoundingClientRect().top;
+		offset = window.pageYOffset - offset;
+		console.log(offset);
 
 		if(isInViewport(container)) {
 			in_or_out[i] = "IN";
@@ -56,25 +61,31 @@ function updateParallaxOffset() {
 			let depth = elements[i].getAttribute('data-depth');
 			let y_offset = offset * depth;
 
-			// if(elements[i].getAttribute('id') == 'trees-front') {
-			// 	y_offset = y_offset + 5;
-			// }
+			//	Debug
 			$(elements[i]).css('transform', `translate(-50%, ${y_offset}px)`);
 			$(elements[i]).css('background-color', 'rgba(0,255,0,0.2)');
 			$(elements[i]).css('border', '5px solid black');
 		}
 		else {
+			//	Debug
 			in_or_out[i] = "OUT";
 			$(elements[i]).css('background-color', 'rgba(255,0,0,0.2)');
 		}
 	}
 
+
+
+	//	Debug start
 	for(let i = 0; i < in_or_out.length - 1; i++) {
 		debug_string = debug_string + in_or_out[i] + ", ";
 	}
+
 	debug_string = debug_string + in_or_out[in_or_out.length - 1];
 	console.log(debug_string);
+	//	Debug end
 }
+
+
 
 //	Update header height to the background parallax layer's height
 function updateHeaderHeight() {
@@ -91,51 +102,38 @@ function updateHeaderHeight() {
 
 
 function isInViewport(el) {
-//	Source: https://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-
-    //	Special bonus for those using jQuery
     if (typeof jQuery === "function" && el instanceof jQuery) {
         el = el[0];
     }
 
+    //	Get element's dimensions.
     var rect = el.getBoundingClientRect();
 
-   	//	If viewport top or bottom is inside rect, return true  ---- Only works if rect is larger than viewport!
    	let viewport_top = 0;
    	let viewport_bot = viewport_top + $(window).height();
 
+   	//	Get height of viewport and parallax container.
    	let viewport_height = viewport_bot - viewport_top;
    	let rect_height = rect.bottom - rect.top;
 
+   	//	Declare some booleans.
    	let top_inside, bot_inside;
 
    	//	Viewport is smaller than parallax container.
    	if(viewport_height <= rect_height) {
+   		//	Check if viewport edges are inside parallax container.
 	   	top_inside = rect.top <= viewport_top && viewport_top <= rect.bottom;
 	   	bot_inside = rect.top <= viewport_bot && viewport_bot <= rect.bottom;
 
-	   	if(!top_inside) {
-	   		console.log("Top out of bounds: " + rect.top + " <= " + viewport_top + " <= " + rect.bottom);
-	   	}
-	   	if(!bot_inside) {
-	   		console.log("Bot out of bounds: " + rect.top + " <= " + viewport_bot + " <= " + rect.bottom);
-	   	}
-
 	   	return (top_inside || bot_inside);
 	}
-	
+
 	//	Parallax container is smaller than viewport.
 	else {
+		//	Check if parallax container edges are inside viewport.
 	   	top_inside = viewport_top < rect.top && rect.top < viewport_bot;
 	   	bot_inside = viewport_top < rect.bottom && rect.bottom < viewport_bot;
 
-	   	if(!top_inside) {
-	   		console.log("Parallax < viewport. Top out of bounds: " + viewport_top + " < " + rect.top + " < " + viewport_bot);
-	   	}
-	   	if(!bot_inside) {
-	   		console.log("Parallax < viewport. Bot out of bounds: " + viewport_top + " < " + rect.bottom + " < " + viewport_bot);
-	   	}
-	   	console.log(top_inside || bot_inside);
 	   	return (top_inside || bot_inside);
 	}
 
