4ab7c8ccfb44fffc3a119b47e432636d6d94e8cc
Misa Jokisalo <misa.jokisalo@gmail.com>
2017-08-06 16:27:28 +0300

    Animation segment test

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+var animData = {
+    container: document.getElementById('bm'),
+    renderer: 'svg',
+    loop: true,
+    prerender: false,
+    autoplay: false,
+    autoloadSegments: false,
+	path: 'img/anim/anim_test2.json',
+	segments: [1,111]
+};
+
+var animation;
+
+animation = bodymovin.loadAnimation(animData);
+animation.addEventListener('DOMLoaded', function(e) {
+	console.log("Animation says DOM loaded");
+	//	Set animation to play first segment
+	animation.playSegments([1,111], true);
+	// animation.play();
+});
+
 $(document).on('click', '.scroll-down', function() {
     $('html, body').animate({
         scrollTop: $('article').offset().top
@@ -5,15 +26,24 @@ $(document).on('click', '.scroll-down', function() {
 });
 
 $(document).ready(function() {
+	//	Initialize ScrollReveal
 	window.sr = ScrollReveal();
 	sr.reveal('.sr');
 });
 
-var animation = bodymovin.loadAnimation({
-	container: document.getElementById('bm'),
-	renderer: 'svg',
-	loop: true,
-	autoplay: true,
-	path: 'img/anim/anim_test.json'
+var alternateAnimation = false;
 
-})
+$(document).on('click', '#bm', function() {
+	if(alternateAnimation) {
+		console.log("First animation");
+		alternateAnimation = false;
+		animation.playSegments([[212,239],[1,111]]);
+	}
+	else {
+		console.log("Second animation");
+		alternateAnimation = true;
+		//	Play transition animation
+		animation.playSegments([[111,141],[141,212]]);
+	}
+	
+});
